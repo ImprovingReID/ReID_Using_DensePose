1c89017a2fd2d59d1f945d5234cfa31d32fd1ead
bjornelwin <bjorn@elwin.com>
2023-02-03 10:12:36 +0100

    Adding first files

---
+++ createUV.py
@@ -0,0 +1,103 @@
+
+import projects.DensePose.apply_net as an
+
+import unpack_IUV 
+import torch 
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
+import numpy as np
+import cv2  
+import os
+from pathlib import Path
+
+import pickle
+configpath = '/mnt/c/users/bjorn/onedrive/dokument/examensarbete/venv/detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml'
+model = 'https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl'
+
+
+def infere_image(image, imdir = None):
+    create_mask(image, imdir)
+    file, dir = create_pkl(image, imdir)
+    texture(pkl_file = file, pkl_dir = dir, directory = imdir)
+
+def infere_images(directory):
+    for image in os.listdir(directory):
+        f = os.path.join(directory, image)
+        # checking if it is a file
+        if os.path.isfile(f):
+            infere_image(image, directory)
+
+def create_pkl(image , directory = None, savedir = 'pkl_files/'):
+    filename = image.split('.')[0]
+    outpath = savedir + filename+ '.pkl'
+    impath = directory + '/' + image
+    
+    parser = an.create_argument_parser()
+    args = parser.parse_args(['dump',configpath, model, impath, '--output',outpath])
+
+    verbosity = args.verbosity if hasattr(args, "verbosity") else None
+    global logger
+    logger = an.setup_logger(name=an.LOGGER_NAME)
+    logger.setLevel(an.verbosity_to_level(verbosity))
+    args.func(args)
+
+    return filename+ '.pkl', savedir
+
+def create_mask(image , directory = None, visualization = 'dp_contour,bbox' , savedir = 'mask/' ): # 'dp_contour,bbox' , 'bbox,dp_segm' 
+    filename = image.split('.')[0]
+    outpath = savedir + filename+ '_mask.jpg'
+    impath = directory + '/' + image
+
+    parser = an.create_argument_parser()
+    args = parser.parse_args(['show',configpath, model, impath, visualization,'-v', '--output',outpath])
+    
+    verbosity = args.verbosity if hasattr(args, "verbosity") else None
+    global logger
+    logger = an.setup_logger(name=an.LOGGER_NAME)
+    logger.setLevel(an.verbosity_to_level(verbosity))
+    args.func(args)
+
+    return savedir + filename + '.jpg'
+
+# def loadall(filename):
+#     with open(filename, "rb") as f:
+#         while True:
+#             try:
+#                 yield pickle.load(f)
+#             except EOFError:
+#                 break
+
+def texture(pkl_file, pkl_dir = None, directory = None, savedir = 'textures/'):
+
+    filename = pkl_file.split('.')[0]
+    outpath = savedir + filename+ '_texture.jpg'
+    impath = directory + '/' + filename + '.jpg'
+
+    with open(pkl_dir + pkl_file, 'rb') as f:
+        data = torch.load(f)
+        # items = loadall(pkl_file)
+    if 'pred_densepose' in  data[0]:
+        i = data[0]['pred_densepose'][0].labels.cpu().numpy()
+        uv = data[0]['pred_densepose'][0].uv.cpu().numpy()
+
+        # I assume the data are stored in pickle, and you are able to read them 
+        results = data[0]
+        iuv = unpack_IUV.parse_iuv(results)
+        bbox = unpack_IUV.parse_bbox(results)
+        image = cv2.imread(impath)[:, :, ::-1]
+        uv_texture = unpack_IUV.get_texture(image, iuv, bbox)
+        
+        if not os.path.exists(savedir):
+            os.makedirs(savedir)
+
+        plt.ioff
+        plt.figure(frameon=False)
+        plt.axis('off')
+        plt.imshow(uv_texture)
+        plt.savefig(outpath)   
+        return outpath
+
+if __name__ == '__main__':
+    infere_images('images')
+    
---
+++ denseSegmentation.py
@@ -0,0 +1,109 @@
+import os
+from matplotlib import pyplot as plt
+import tqdm
+import createUV as cuv
+import cv2
+from pathlib import Path
+
+# def _crop(im, bbox):
+#     ih, iw, _ = im.shape
+#     b = [bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1]]
+#     x, y, w, h = [int(v * r) for v, r in zip(b, [iw, ih, iw, ih])]
+#     return im[y : y + h, x : x + w, :]
+
+# def _stuff(im, idx, output_path):
+    
+#     if texs == None or len(texs) == 0:
+#         print("warning: no detection in crop")
+#         return None 
+    
+#     p = f"{output_path}/uv_maps/{idx:06}.jpg"
+#     texs[0]["texture"].save_to_file(p)
+#     cv2.imwrite(f"{output_path}/crops/{idx:06}.jpg", im)
+#     return p
+    
+# def parse_train_json(train, data_dir, output_dir):
+#     for person in tqdm.tqdm(train):
+#         person_id = person["person_id"].replace("/", "-")       
+        
+#         person_dir = output_dir / person_id
+        
+#         if not os.path.isdir(person_dir):
+#             Path(person_dir + '/' + "crops").mkdir(exist_ok=True, parents=True)  
+#             Path(person_dir + '/' + "pkl_files").mkdir(exist_ok=True, parents=True)  
+#             Path(person_dir + '/' + "uv_maps").mkdir(exist_ok=True, parents=True)    
+        
+#         for idx, sample in enumerate(person["samples"]):
+#             im = cv2.imread(str(data_dir / sample["image_path"]))
+#             bbox = [sample[k] for k in ("xtl", "ytl", "xbr", "ybr")]
+#             crop = _crop(im, bbox)          
+            
+#             p = _stuff(uv_mapper, crop, idx, person_dir)
+#             if p is not None:
+#                 sample["uv_image_path"] = p
+    
+
+
+def denseSegmentor(dataset_dir,output_dir, crop = False):
+    
+    Path(output_dir).mkdir(exist_ok=True, parents=True)
+
+    for image in os.listdir(dataset_dir):
+        f = os.path.join(dataset_dir, image)
+        if os.path.isfile(f):
+
+            person_id = image[0:4]
+            person_dir = output_dir + '/' + person_id
+            # if os.path.isdir(person_dir):
+                # print(f"skipping {person_id} because it already exists")       
+                
+            # else:
+            if not os.path.isdir(person_dir):
+                Path(person_dir + '/' + "crops").mkdir(exist_ok=True, parents=True)  
+                Path(person_dir + '/' + "pkl_files").mkdir(exist_ok=True, parents=True)  
+                Path(person_dir + '/' + "uv_maps").mkdir(exist_ok=True, parents=True)
+
+            # save crop
+            crop = cv2.imread(f)
+            cv2.imwrite(person_dir + '/' + "crops" + '/' + image, crop)  
+            
+            #create and save pkl files
+            pkl_file , pkl_dir = cuv.create_pkl(image,directory = person_dir + '/' + "crops"  ,savedir = person_dir + '/' + "pkl_files/") 
+            
+            #create and save uv maps
+            cuv.texture(pkl_file , pkl_dir,directory = person_dir + '/' + "crops"  ,savedir = person_dir + '/' + "uv_maps/")
+
+
+def market1501():
+    output_dir= 'SegmentedMarket1501'
+    
+    Path(output_dir).mkdir(exist_ok=True, parents=True)
+
+    images_dir = '../Market-1501-v15.09.15/gt_bbox'
+    for image in os.listdir(images_dir):
+        f = os.path.join(images_dir, image)
+        if os.path.isfile(f):
+
+            person_id = image[0:4]
+            person_dir = output_dir + '/' + person_id
+            # if os.path.isdir(person_dir):
+                # print(f"skipping {person_id} because it already exists")       
+                
+            # else:
+            if not os.path.isdir(person_dir):
+                Path(person_dir + '/' + "crops").mkdir(exist_ok=True, parents=True)  
+                Path(person_dir + '/' + "pkl_files").mkdir(exist_ok=True, parents=True)  
+                Path(person_dir + '/' + "uv_maps").mkdir(exist_ok=True, parents=True)
+
+            # save crop
+            crop = cv2.imread(f)
+            cv2.imwrite(person_dir + '/' + "crops" + '/' + image, crop)  
+            
+            #create and save pkl files
+            pkl_file , pkl_dir = cuv.create_pkl(image,directory = person_dir + '/' + "crops"  ,savedir = person_dir + '/' + "pkl_files/") 
+            
+            #create and save uv maps
+            cuv.texture(pkl_file , pkl_dir,directory = person_dir + '/' + "crops"  ,savedir = person_dir + '/' + "uv_maps/")
+
+if __name__ == '__main__':    
+    market1501()
