29ba218ebb41d780f3f61792bc21f8baa89e6b11
Björn Elwin <bjornel@pc40308-2031.se.axis.com>
2023-02-21 11:38:53 +0100

    market

--- Re-ID/Market1501.py
+++ Re-ID/Market1501.py
@@ -61,14 +61,73 @@ class Market1501(Dataset):
         img = self.trans(img)
         return img, self.lb_ids[idx], self.lb_cams[idx]
 
+class DensePose1501(Dataset):
+    '''
+    a wrapper of Market1501 densepose dataset
+    '''
+    def __init__(self, data_path, is_train = True, *args, **kwargs):
+        super(DensePose1501, self).__init__(*args, **kwargs)
+        self.is_train = is_train
+        self.data_path = data_path
+        self.imgs = os.listdir(data_path)
+        self.imgs = [el for el in self.imgs if os.path.splitext(el)[1] == '.jpg']
+        self.lb_ids = [int(el.split('_')[0]) for el in self.imgs]
+        self.lb_cams = [int(el.split('_')[1][1]) for el in self.imgs]
+        self.imgs = [os.path.join(data_path, el) for el in self.imgs]
+        if is_train:
+            self.trans = transforms.Compose([
+                transforms.Resize((288, 144)),
+                transforms.RandomCrop((256, 128)),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.486, 0.459, 0.408), (0.229, 0.224, 0.225)),
+                RandomErasing(0.5, mean=[0.0, 0.0, 0.0])
+            ])
+        else:
+            self.trans_tuple = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.486, 0.459, 0.408), (0.229, 0.224, 0.225))
+                ])
+            self.Lambda = transforms.Lambda(
+                lambda crops: [self.trans_tuple(crop) for crop in crops])
+            self.trans = transforms.Compose([
+                transforms.Resize((288, 144)),
+                transforms.TenCrop((256, 128)),
+                self.Lambda,
+            ])
+
+        # useful for sampler
+        self.lb_img_dict = dict()
+        self.lb_ids_uniq = set(self.lb_ids)
+        lb_array = np.array(self.lb_ids)
+        for lb in self.lb_ids_uniq:
+            idx = np.where(lb_array == lb)[0]
+            self.lb_img_dict.update({lb: idx})
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.imgs[idx])
+        print(type(img))
+        img = self.trans(img)
+        print(type(img))        
+        return img, self.lb_ids[idx], self.lb_cams[idx]
 
 
 if __name__ == "__main__":
-    ds = Market1501('/mnt/analyticsvideo/DensePoseData/market1501Market-1501-v15.09.15/bounding_box_train', is_train = True)
-    im, _, _ = ds[1]
+    ds = Market1501('/mnt/analyticsvideo/DensePoseData/market1501/Market-1501-v15.09.15/bounding_box_train', is_train = True)
+    ds2 = DensePose1501('/home/bjornel/market1501/SegmentedMarket1501train/uv_maps/uv', is_train = True)
+    im, _, _ = ds2[0]
     print(im.shape)
     print(im.max())
     print(im.min())
+    # import numpy
+    # import matplotlib.pyplot as plt
+    # img = im.numpy().transpose(1, 2, 0)
+    # print(img.shape)
+    # plt.imshow(img)
+    # plt.show()
     ran_er = RandomErasing()
     im = ran_er(im)
     cv2.imshow('erased', im)
